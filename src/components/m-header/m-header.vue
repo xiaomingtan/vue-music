8e4fd98e9ad6809c38590fecb5fe6627ac4ac785
[xiaomingtan] <[15521372445@sina.cn]>
2017-07-14 00:02:00 +0800

    m-header css

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="m-header">
+        <div class="icon"></div>
+        <h1 class="title">Chicken Music</h1>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+
+    }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+    @import "../../common/stylus/mixin.styl"
+    @import "../../common/stylus/variable.styl"
+
+    .m-header
+        position: relative
+        height 44px
+        background-color : $color-theme
+        text-align : center
+        font-size: 0
+        .icon
+            display: inline-block
+            vertical-align : top
+            margin: 4px 9px 0 0
+            width: 30px
+            height: 32px
+            background-size: 30px 32px
+            bg-image('./logo')
+        .title
+            vertical-align: top
+            line-height: 44px
+            font-size: $font-size-large
+            display: inline-block
+
+</style>
