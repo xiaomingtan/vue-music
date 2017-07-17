19d6de0adb3c9aa8df1442755e7b2fea8fe99a8e
unknown <tanjl@Development.tec>
2017-07-17 19:02:44 +0800

    get recommend data

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,30 @@
+/**
+ * Created by tanjl on 2017/7/17.
+ */
+import originJsonp from 'jsonp';
+
+export default function jsonp (url, data, option) {
+    url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+
+    return new Promise((resolve, reject) => {
+        originJsonp(url, option, (error, data) => {
+            if (!error) {
+                resolve(data)
+            } else {
+                reject(error)
+            }
+        });
+    })
+
+}
+
+function param(data) {
+    let param = '';
+    for (var key in data) {
+        let value = data[key] !== undefined ? data[key] : ''
+        param += `&${key}=${value}`
+    }
+
+    return param ? param.substring(1) : param;
+
+}
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -5,7 +5,24 @@
 </template>
 
 <script type="text/ecmascript-6">
-    export default {}
+    import {ERROR_OK} from '@/api/config'
+    import {getRecommend} from '@/api/recommend'
+    export default {
+        created() {
+            this._getRecommend();
+        },
+        methods: {
+            _getRecommend() {
+               getRecommend().then( (res) => {
+                    if (res.code == ERROR_OK) {
+                        console.log(res)
+                    } else {
+                        console.log(res.code)
+                    }
+               })
+            }
+        }
+    }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
